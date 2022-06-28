3c50835896a30e0d3b87e96c454d0679b7f138ff
JoaoLCom <joao@compliasset.com>
2022-06-28 10:44:13 -0300

    to be continue

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :costumers
   resources :ecom_orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'ecom_orders#index'
+  root 'home#index'
 end
